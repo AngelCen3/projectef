dc8ea11c9683bdd05fc48c350d1b62dca97b2c04
BetoRobert <wyjawy@hotmail.com>
2024-11-26 08:00:34 -0500

    DB in Memory for the moment

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef;
+
+var builder = WebApplication.CreateBuilder(args);  // Cofiguramos entity framework 
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+// Haremos una prueba de una conexion en Memory
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory()); //Nos da true o false
+});
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,19 @@
+# Curso de Fundamentos de Entity Framework
+
+By Miguel Teheran
+
+En este ejemplo trabajaremos con modelos el modelo
+
+- Category
+
+- Task
+
+Hasta este momento y me refiero a los videos del curso (video 8)
+solo con cambiar la biblioteca instalada Entity Framework a la
+biblioteca de base de datos bastaria con poder cambiar de la misma
+
+Por ejemplo este es para slq server
+
+```bash
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 9.0.0
+```
